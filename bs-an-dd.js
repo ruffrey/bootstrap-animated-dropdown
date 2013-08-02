1ee1fda181bfafe5ba89fab04e199cf2d1129027
ruffrey <jpx@wiggleware.com>
2013-08-01 20:51:13 -0700

    support for mobile devices that cannot do hover
    Devices did not trigger the hover event so the menu was useless on them. Now it has a click function to override the hover.

--- bs-an-dd.js
+++ bs-an-dd.js
@@ -1,14 +1,46 @@
-function BindAnimatedDropdown($dd) {
-	$dd.find('li.dropdown').each(function(){
+var l; // needs a global variable for timeout
+function BindMenu($m) {
+	$m.find('li.dropdown').each(function(){
 		var $li = $(this);
 		$li.hover(function(){
 			clearTimeout(l);
-			$li.addClass('active').children('ul').slideDown(200);
-			$('li.dropdown').not($li).removeClass('active').children('ul').fadeOut(400);
+			if(!$('.active-click').length)
+			{
+				$li.addClass('active').children('ul').slideDown(200);
+				$('li.dropdown').not($li).removeClass('active').children('ul').fadeOut(400);
+			}
 		}, function(){
-			l = setTimeout(function(){
-				$li.removeClass('active').children('ul').fadeOut(400);
-			},300);
+			if(!$('.active-click').length)
+			{
+				l = setTimeout(function(){
+					$li.removeClass('active').children('ul').fadeOut(400);
+				},300);
+			}
 		});
+		$li.click(function(e){
+			e.preventDefault();
+			clearTimeout(l);
+			if($li.hasClass('active-click'))
+			{
+				$li
+				.removeClass('active')
+				.children('ul')
+				.fadeOut(400);
+				$('.active-click').removeClass('active-click');
+			}
+			else{
+				$li
+				.addClass('active-click')
+				.addClass('active')
+				.children('ul')
+				.slideDown(200);
+				$('li.dropdown').not($li).removeClass('active').children('ul').fadeOut(400);
+			}
+		});
+		
+		$li.find('li').click(function(e){
+			e.stopPropagation();
+		});
+		
 	});
 }
